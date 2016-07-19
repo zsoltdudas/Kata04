7679a4df55287968f8203067593d459a279b1aa5
Zsolt Dudás <zdudas@cloudbasesolutions.com>
2016-07-19 13:38:04 +0300

    Removed filename variable

--- data_munging.py
+++ data_munging.py
@@ -7,15 +7,13 @@ class data_munging(unittest.TestCase, file_handling):
         self.spread = []
 
     def test_weather(self):
-        filename = "weather.dat"
-        weather_data = self.read_file(filename)
+        weather_data = self.read_file("weather.dat")
         smallest = self.smallest_difference(weather_data, 1, 2)
 
         print "Day with smallest spread: ", smallest + 1
 
     def test_football(self):
-        filename = "football.dat"
-        league = self.read_file(filename)
+        league = self.read_file("football.dat")
         smallest = self.smallest_difference(league, 6, 8)
 
         print "Smallest goal difference: ", league[smallest][1]
